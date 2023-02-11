499eb9f0c3cce3fdad51c8806a45c11d24f7bfc3
kodi1993 <Akhmedka1993@gmail.com>
2023-02-11 17:43:42 +0300

    Fix KeyErrors, Change F to C, Add pictures for Sky Condition

--- backend.py
+++ backend.py
@@ -3,7 +3,7 @@ import requests
 API_KEY = "1a223444ed3adcccd618a5d9c26de3a1"
 
 
-def get_data(place, forecast_days=None, data_to_display=None):
+def get_data(place, forecast_days=None):
     url = f"http://api.openweathermap.org/data/2.5/forecast?q={place}" \
           f"&APPID={API_KEY}"
     number_of_observations = forecast_days * 8
@@ -11,10 +11,6 @@ def get_data(place, forecast_days=None, data_to_display=None):
     data = response.json()
     filtered_data = data["list"]
     filtered_data = filtered_data[:number_of_observations]
-    if data_to_display == "Temperature":
-        filtered_data = [dict["main"]["temp"] for dict in filtered_data]
-    if data_to_display == "Sky":
-        filtered_data = [dict["weather"][0]["main"] for dict in filtered_data]
     return filtered_data
 
 
--- main.py
+++ main.py
@@ -10,13 +10,26 @@ data_to_display = st.selectbox(
     'Select data to view:',
     ('Temperature', 'Sky'))
 st.subheader(f'{data_to_display} for the next {days} days in {place}')
+if place:
+    try:
+        filtered_data = get_data(place, days)
 
-data = get_data(place, days, data_to_display)
+        if data_to_display == "Temperature":
+            temperatures = [((dict["main"]["temp"] / 10) - 32) * (5 / 9) for dict in filtered_data]
+            dates = [dict["dt_txt"] for dict in filtered_data]
+            figure = px.line(x=dates, y=temperatures,
+                             labels={'x': f'Days',
+                                     'y': 'Temperature'})
+            st.plotly_chart(figure)
+        if data_to_display == "Sky":
+            sky_conditions = [dict["weather"][0]["main"] for dict in filtered_data]
+            list_of_paths = []
+            for condition in sky_conditions:
+                image_path = f"images/{condition.lower()}.png"
+                list_of_paths.append(image_path)
 
-st.write(data)
+            st.image(list_of_paths, width=100)
+    except KeyError:
+        st.write("The place you entered does not exist")
 
-#figure = px.scatter(x=df[x_axis], y=df[y_axis],
-              #      labels={'x': f'{x_axis.replace("_", " ").title()}',
-                            'y': f'{y_axis.replace("_", " ").title()}'})
-#st.plotly_chart(figure)
 
